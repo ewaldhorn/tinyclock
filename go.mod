b7938c4a6deed3a37c1915712c062f087df7d868
Ewald Horn <ewaldhorn@gmail.com>
2025-01-24 09:36:38 +0200

    Updated to use the latest TinyCanvas v0.0.9 improvements

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.23.5
 
 require (
 	github.com/ewaldhorn/dommie v0.0.2
-	github.com/ewaldhorn/tinycanvas v0.0.8
+	github.com/ewaldhorn/tinycanvas v0.0.9
 )
--- src/effect.go
+++ src/effect.go
@@ -13,80 +13,100 @@ func setupAnimation() {
 
 // ----------------------------------------------------------------------------
 func render() {
-	ctx.Save() //a
-	ctx.ClearRect(0, 0, canvasWidth, canvasHeight)
-	ctx.Translate(75, 75)
-	ctx.Call("scale", 0.4, 0.4)
-	ctx.Rotate(-math.Pi / 2)
-	ctx.StrokeStyle("black")
-	ctx.FillStyle("white")
-	ctx.LineWidth(8)
-	ctx.Set("lineCap", "round")
-
-	// hour marks
-	ctx.Save() //b
-	for range 12 {
-		ctx.BeginPath()
-		ctx.Rotate(math.Pi / 6)
-		ctx.Call("moveTo", 100, 0)
-		ctx.LineTo(120, 0)
-		ctx.Stroke()
-	}
-	ctx.Restore() //b
-
-	// minutes
-	ctx.Save() //c
-	ctx.LineWidth(5)
-	for i := range 60 {
-		if i%5 != 0 {
-			ctx.BeginPath()
-			ctx.Call("moveTo", 117, 0)
-			ctx.LineTo(120, 0)
-			ctx.Stroke()
-		}
-		ctx.Rotate(math.Pi / 30)
-	}
-	ctx.Restore() //c
+	prepareClockFace()
+	renderHourMarks()
+	renderMinuteMarks()
 
 	now := time.Now()
 	sec := float64(now.Second())
 	min := float64(now.Minute())
 	hour := float64(now.Hour() % 12)
 
-	ctx.FillStyle("black")
-
 	// TODO support
 	// canvas.innerText = `The time is: ${hr}:${min}`;
 
-	// Write Hours
+	ctx.FillStyle("black")
+	renderHours(hour, min, sec)
+	renderMinutes(min, sec)
+	renderSeconds(sec)
+	renderClockOutline()
+}
+
+// ----------------------------------------------------------------------------
+func prepareClockFace() {
+	ctx.Save() //a
+	ctx.ClearRect(0, 0, canvasWidth, canvasHeight)
+	ctx.Translate(75, 75)
+	ctx.Scale(0.4, 0.4)
+	ctx.Rotate(-math.Pi / 2)
+	ctx.StrokeStyle("black")
+	ctx.FillStyle("white")
+	ctx.LineWidth(8)
+	ctx.LineCap("round")
+}
+
+// ----------------------------------------------------------------------------
+func renderHourMarks() {
+	ctx.Save() //b
+	for range 12 {
+		ctx.BeginPath()
+		ctx.Rotate(math.Pi / 6)
+		ctx.MoveTo(100, 0)
+		ctx.LineTo(120, 0)
+		ctx.Stroke()
+	}
+	ctx.Restore() //b
+}
+
+// ----------------------------------------------------------------------------
+func renderMinuteMarks() {
+	ctx.Save() //c
+	ctx.LineWidth(5)
+	for i := range 60 {
+		if i%5 != 0 {
+			ctx.BeginPath()
+			ctx.MoveTo(117, 0)
+			ctx.LineTo(120, 0)
+			ctx.Stroke()
+		}
+		ctx.Rotate(math.Pi / 30)
+	}
+	ctx.Restore() //c
+}
+
+// ----------------------------------------------------------------------------
+func renderHours(hour, min, sec float64) {
 	ctx.Save() //d
 	ctx.Rotate(math.Pi/6.0*hour + (math.Pi/360.0)*min + (math.Pi/21600)*sec)
 	ctx.LineWidth(14)
 	ctx.BeginPath()
-	ctx.Call("moveTo", -20, 0)
+	ctx.MoveTo(-20, 0)
 	ctx.LineTo(80, 0)
 	ctx.Stroke()
 	ctx.Restore() //d
+}
 
-	// Minutes
-	// Write Minutes
+// ----------------------------------------------------------------------------
+func renderMinutes(min, sec float64) {
 	ctx.Save() //e
 	ctx.Rotate((math.Pi/30)*min + (math.Pi/1800)*sec)
 	ctx.LineWidth(10)
 	ctx.BeginPath()
-	ctx.Call("moveTo", -28, 0)
+	ctx.MoveTo(-28, 0)
 	ctx.LineTo(112, 0)
 	ctx.Stroke()
 	ctx.Restore() //e
+}
 
-	// Seconds
+// ----------------------------------------------------------------------------
+func renderSeconds(sec float64) {
 	ctx.Save() //f
 	ctx.Rotate((sec * math.Pi) / 30)
 	ctx.StrokeStyle("#D40000")
 	ctx.FillStyle("#D40000")
 	ctx.LineWidth(6)
 	ctx.BeginPath()
-	ctx.Call("moveTo", -30, 0)
+	ctx.MoveTo(-30, 0)
 	ctx.LineTo(83, 0)
 	ctx.Stroke()
 	ctx.BeginPath()
@@ -99,7 +119,10 @@ func render() {
 	ctx.Arc(0, 0, 3, 0, math.Pi*2, true)
 	ctx.Fill()
 	ctx.Restore() //f
+}
 
+// ----------------------------------------------------------------------------
+func renderClockOutline() {
 	ctx.BeginPath()
 	ctx.LineWidth(14)
 	ctx.StrokeStyle("#325FA2")
